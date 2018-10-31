5753869a77f4e33757f41859184d73133f521fd3
Daniele Viti <dnviti@gmail.com>
2018-11-01 00:19:48 +0100

    Add files via upload

--- views/page.php
+++ views/page.php
@@ -40,6 +40,9 @@ class Page
             case 6:
                 echo $this->lista_utenti($pnum);
                 break;
+            case 3:
+                echo $this->lista_incassi($pnum);
+                break;
         }
     }
 
@@ -103,8 +106,8 @@ class Page
         $_templates = new Template();
 
         $gridRow_btn = [
-            $_components->buttonPrimary("Nuovo Incasso"),
-            $_components->buttonSecondary("Lista Abbonati")
+            $_components->button("Nuovo Incasso","Primary","2"),
+            $_components->button("Lista Utenti","Secondary","6")
         ];
 
         $footer_objs = [
@@ -119,7 +122,7 @@ class Page
             . $_templates->slideMenu()
             . $_templates->body()
             . $_components->hGridRow($gridRow_btn, 'btnNav')
-            . $_components->tableFromQuery('query_report_homepage_where', 'tbAbbon', 'Ultimi Incassi')
+            . $_components->tableFromQuery('query_report_homepage_where', 'tbAbbon', 'Ingressi Autorizzati Oggi')
             . $_components->javaScriptFromFile('slidemenu')
             . $_templates->footer($footer_objs);
 
@@ -133,7 +136,7 @@ class Page
         $_templates = new Template();
 
         $gridRow_btn = [
-            $_components->buttonPrimary("Nuovo Utente")
+            $_components->button("Nuovo Utente","Primary","7")
         ];
 
         $footer_objs = [
@@ -149,6 +152,35 @@ class Page
             . $_templates->body()
             . $_components->hGridRow($gridRow_btn, 'btnNav')
             . $_components->tableFromQuery('query_anagrafica_utenti', 'tbAbbon', 'Lista Utenti')
+            . $_components->javaScriptFromFile('slidemenu')
+            . $_templates->footer($footer_objs);
+
+        return $page;
+    }
+
+
+    function lista_incassi($pnum)
+    {
+        $_components = new Component();
+        $_templates = new Template();
+
+        $gridRow_btn = [
+            $_components->button("Nuovo Incasso","Primary","2")
+        ];
+
+        $footer_objs = [
+            // generazione Menu (codice in variabile pubblica di classe)
+            $_components->javaScript($this->menuJs),
+            // colorazione menu attivo
+            $_components->javaScript('$("#m-p' . $pnum . '").addClass("active")')
+        ];
+
+        $page = ''
+            . $_templates->header()
+            . $_templates->slideMenu()
+            . $_templates->body()
+            . $_components->hGridRow($gridRow_btn, 'btnNav')
+            . $_components->tableFromQuery('query_report_completo', 'tbAbbon', 'Lista Utenti')
             . $_components->javaScriptFromFile('slidemenu')
             . $_templates->footer($footer_objs);
 
